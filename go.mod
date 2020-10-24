ffca2266c563bd9c16282ca73d6003b8a627bd98
nugroho samiyono <nugrohosamiyono@gmail.com>
2020-10-24 12:30:04 +0700

    Initiate | Go stream video

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nugrohosam/govideostream
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	http.Handle("/", handlers())
+	http.ListenAndServe(":8000", nil)
+}
+
+func handlers() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexPage).Methods("GET")
+	router.HandleFunc("/media/{mID:[0-9]+}/stream/", streamHandler).Methods("GET")
+	router.HandleFunc("/media/{mID:[0-9]+}/stream/{segName:index[0-9]+.ts}", streamHandler).Methods("GET")
+	return router
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func streamHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	mID, err := strconv.Atoi(vars["mID"])
+	if err != nil {
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	segName, ok := vars["segName"]
+	if !ok {
+		mediaBase := getMediaBase(mID)
+		m3u8Name := "index.m3u8"
+		serveHlsM3u8(response, request, mediaBase, m3u8Name)
+	} else {
+		mediaBase := getMediaBase(mID)
+		serveHlsTs(response, request, mediaBase, segName)
+	}
+}
+
+func getMediaBase(mID int) string {
+	mediaRoot := "./assets/media"
+	return fmt.Sprintf("%s/%d", mediaRoot, mID)
+}
+
+func serveHlsM3u8(w http.ResponseWriter, r *http.Request, mediaBase, m3u8Name string) {
+	mediaFile := fmt.Sprintf("%s/hls/%s", mediaBase, m3u8Name)
+	http.ServeFile(w, r, mediaFile)
+	w.Header().Set("Content-Type", "application/x-mpegURL")
+}
+
+func serveHlsTs(w http.ResponseWriter, r *http.Request, mediaBase, segName string) {
+	mediaFile := fmt.Sprintf("%s/hls/%s", mediaBase, segName)
+	http.ServeFile(w, r, mediaFile)
+	w.Header().Set("Content-Type", "video/MP2T")
+}
